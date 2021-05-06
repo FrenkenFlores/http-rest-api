68fb7460489bbe427452821e5b0d054c031e98ad
FrenkenFlores <fflores@student.21-school.ru>
2021-05-06 04:14:17 +0300

    update #3: http server is up

--- internel/app/apiserver/apiserver.go
+++ internel/app/apiserver/apiserver.go
@@ -1,20 +1,25 @@
 package apiserver
 
 import (
+	"io"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
 type APIserver struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 func New(new_config *Config) *APIserver {
 	return &APIserver{
 		logger: logrus.New(),
 		config: new_config,
+		router: mux.NewRouter(),
 	}
 }
 
@@ -22,8 +27,9 @@ func (s *APIserver) Start() error {
 	if err := s.ConfigureLogger(); err != nil {
 		log.Fatal(err)
 	}
+	s.ConfigureRouter()
 	s.logger.Info("Starting API server")
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIserver) ConfigureLogger() error {
@@ -33,4 +39,14 @@ func (s *APIserver) ConfigureLogger() error {
 	}
 	s.logger.SetLevel(level)
 	return nil
+}
+
+func (s *APIserver) ConfigureRouter() {
+	s.router.HandleFunc("/hello", s.HelloFunc())
+}
+
+func (s *APIserver) HelloFunc() http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		io.WriteString(rw, "Hello, FrenkenFlores!")
+	}
 }
